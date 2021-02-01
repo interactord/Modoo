60c2779e7ef5729d0b2c68dc7eae708fce5e36aa
Scott Moon <interactord@gmail.com>
2021-02-02 01:35:22 +0900

    추가 -> 그라데이션 배경을 사용할 수 있는 ASDisplayNode 확장
    - Texture의 DisplayNode는 일반 뷰와 달리 비동기로 해당 그라이디언트를 구현해야합니다.

---
+++ Projects/Application/Sources/Extensions/Texture/ASDIsplayNode+Gradient.swift
@@ -0,0 +1,48 @@
+import AsyncDisplayKit
+
+extension ASDisplayNode {
+
+  // MARK: Internal
+
+  enum GradientDirection {
+    case horizontal
+    case vertical
+    case adjust(CGPoint, CGPoint)
+
+    var point: (start: CGPoint, end: CGPoint) {
+      switch self {
+      case .horizontal:
+        return (start: .init(x: .zero, y: 0.5), end: .init(x: 1.0, y: 0.5) )
+      case .vertical:
+        return (start: .init(x: 0.5, y: .zero), end: .init(x: 0.5, y: 1.0))
+      case let .adjust(start, end):
+        return (start: start, end: end)
+      }
+    }
+  }
+
+  func gradientBackgroundColor(colors: [CGColor], direction: GradientDirection) {
+    guard hasEmptyGradientLayer else { return }
+
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else { return }
+      self.backgroundColor = .clear
+      self.clipsToBounds = true
+
+      let gradient = CAGradientLayer()
+      gradient.frame = .init(origin: .zero, size: self.calculatedSize)
+      gradient.colors = colors
+      gradient.startPoint = direction.point.start
+      gradient.endPoint = direction.point.end
+
+      self.layer.insertSublayer(gradient, at: .zero)
+    }
+  }
+
+  // MARK: Private
+
+  private var  hasEmptyGradientLayer: Bool {
+    guard let _ = layer.sublayers?.first(where: { $0 is CAGradientLayer }) else { return  false }
+    return true
+  }
+}
