c38c53ec9319f30aee4c2663170462e535891195
Scott Moon <interactord@gmail.com>
2021-02-07 00:59:34 +0900

    추가 -> text 입력 폼 뷰, 리엑터
    - 회원가입 또는 로그인에 사용할 text 입력폼 뷰입니다.
    - 리액터는 비밀번호와 자릿수를 Node에서 받아 검증하는 역할로 사용됩니다.

---
+++ Projects/Application/Tests/ComponentNodes/Form/FormTextInputNode/FormTextInputReactorSpec.swift
@@ -0,0 +1,122 @@
+import Nimble
+import Quick
+
+@testable import Application
+
+class FormTextInputReactorSpec: QuickSpec {
+  override func spec() {
+    typealias Status = FormTextInputReactor.Status
+    // swiftlint:disable implicitly_unwrapped_optional
+    var reactor: FormTextInputReactor!
+
+    let emailScope = FormTextInputReactor.Scope.email
+    let passwordScope = FormTextInputReactor.Scope.password
+    let plainScope = FormTextInputReactor.Scope.plain(placeholderString: "test")
+
+    beforeEach {
+      reactor = FormTextInputReactor()
+    }
+    afterEach {
+      reactor = nil
+    }
+
+    describe("FormTextInputReactor 이메일 검증테스트") {
+
+      context("빈 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(emailScope, ""))
+        }
+
+        it("reactor currentState statue는 wrong 타입이다") {
+          expect(reactor.currentState.statue) == Status.wrong
+        }
+      }
+
+      context("잘못된 이메일 형식의 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(emailScope, "1231231"))
+        }
+
+        it("reactor currentState statue는 wrong 타입이다") {
+          expect(reactor.currentState.statue) == Status.wrong
+        }
+      }
+
+      context("정상적인 이메일 형식의 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(emailScope, "asdvb@asdf.com"))
+        }
+
+        it("reactor currentState statue는 valid 타입이다") {
+          expect(reactor.currentState.statue) == Status.valid
+        }
+      }
+    }
+
+    describe("FormTextInputReactor 패스워드 검증테스트") {
+
+      context("빈 텍스트 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(passwordScope, ""))
+        }
+
+        it("reactor currentState statue는 wrong 타입이다") {
+          expect(reactor.currentState.statue) == Status.wrong
+        }
+      }
+
+      context("6자리 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(passwordScope, "123456"))
+        }
+
+        it("reactor currentState statue는 wrong 타입이다") {
+          expect(reactor.currentState.statue) == Status.wrong
+        }
+      }
+
+      context("7자리 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(passwordScope, "1234567"))
+        }
+
+        it("reactor currentState statue는 valid 타입이다") {
+          expect(reactor.currentState.statue) == Status.valid
+        }
+      }
+    }
+
+    describe("FormTextInputReactor 일반텍스트 검증테스트") {
+
+      context("빈 텍스트 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(plainScope, ""))
+        }
+
+        it("reactor currentState statue는 wrong 타입이다") {
+          expect(reactor.currentState.statue) == Status.wrong
+        }
+      }
+
+      context("3자리 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(plainScope, "123"))
+        }
+
+        it("reactor currentState statue는 wrong 타입이다") {
+          expect(reactor.currentState.statue) == Status.wrong
+        }
+      }
+
+      context("4자리 텍스트를 보내면") {
+        beforeEach {
+          reactor.action.onNext(.editingChanged(plainScope, "1234"))
+        }
+
+        it("reactor currentState statue는 valid 타입이다") {
+          expect(reactor.currentState.statue) == Status.valid
+        }
+      }
+    }
+  }
+}
