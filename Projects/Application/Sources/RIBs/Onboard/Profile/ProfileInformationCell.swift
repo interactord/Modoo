6aaec6eee4b5b33faa2d9713cd3b25c62dd211af
Scott Moon <interactord@gmail.com>
2021-02-14 02:14:06 +0900

    추가 -> 프로필 요약 정보 셀 추가

---
+++ Projects/Application/Sources/RIBs/Onboard/Profile/ProfileInformationCell.swift
@@ -0,0 +1,53 @@
+import AsyncDisplayKit
+import BonMot
+
+// MARK: - ProfileInformationCell
+
+final class ProfileInformationCell: ASCellNode {
+
+  // MARK: Lifecycle
+
+  override init() {
+    super.init()
+    automaticallyManagesSubnodes = true
+    backgroundColor = .white
+  }
+
+  deinit {
+    print("ProfileInformationCell deinit...")
+  }
+
+  // MARK: Internal
+
+  let summeryNode: ProfileInformationSummeryNode = {
+    ProfileInformationSummeryNode(
+      postCount: "213",
+      followerCount: "863",
+      followingCount: "408")
+  }()
+
+  // MARK: Private
+
+  private struct Const {
+    static let contentPadding = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+  }
+}
+
+// MARK: - LayoutSpec
+
+extension ProfileInformationCell {
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    style.width = .init(unit: .fraction, value: 1.0)
+
+    let contentLayout = ASStackLayoutSpec(
+      direction: .vertical,
+      spacing: .zero,
+      justifyContent: .start,
+      alignItems: .stretch,
+      children: [
+        summeryNode,
+      ])
+
+    return ASInsetLayoutSpec(insets: Const.contentPadding, child: contentLayout)
+  }
+}
