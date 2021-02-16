f47da83dfb9d770092eaa6d6175218b9d0e51b5f
Scott Moon <interactord@gmail.com>
2021-02-16 15:29:48 +0900

    추가 -> SingleHeaderSection, ProfileViewController적용
    - 현재 개발단계에서는 헤더만 필요한 상태라 각 섹션에 맞춰 헤더 셀을 사용하는 SectionController를 추가하였습니다.

--- Projects/Application/Tests/RIBs/Onboard/Profile/ProfileViewControllerSpec.swift
+++ Projects/Application/Tests/RIBs/Onboard/Profile/ProfileViewControllerSpec.swift
@@ -1,5 +1,6 @@
 import Nimble
 import Quick
+import RxSwift
 @testable import Application
 
 class ProfileViewControllerSpec: QuickSpec {
@@ -23,19 +24,6 @@ class ProfileViewControllerSpec: QuickSpec {
       context("더미 아이템을 1개를 담을 경우") {
         beforeEach {
           viewController.items = Array(repeating: "A", count: 1)
-        }
-
-        it("컬렉션뷰의 섹션은 1개이다") {
-          expect(viewController.numberOfSections(in: viewController.node.collectionNode)) == 1
-        }
-
-        it("컬렉션뷰의 첫번째 섹현의 아이템은 3개이다") {
-          expect(viewController.collectionNode(viewController.node.collectionNode, numberOfItemsInSection: 0)) == 1
-        }
-
-        it("컬렉션뷰의 1번쨰 색션의 1번째 아이템은 FeedPostCellNode이다") {
-          let cellNode = viewController.collectionNode(viewController.node.collectionNode, nodeBlockForItemAt: [0, 0])()
-          expect(cellNode is ProfileInformationCell).to(beTrue())
         }
       }
     }
