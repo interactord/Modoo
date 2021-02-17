6fd9e15f013af17b02db3ba1adfc2c3d514190ee
Scott Moon <interactord@gmail.com>
2021-02-17 10:51:34 +0900

    버그수정 -> 컬렉션 DB, ProfileCell Width
    - 컬렉션 DB의 경우, 이미지 업로드 경로가 올바르지 않아, 올바르게 수정하였습니다.
    - ProfileCell Width의 경우는, 연산식이 잘못되어 수정하였습니다.

--- Projects/Application/Sources/Platforms/FirebaseAPIPlatform/UseCase/FirebaseAuthenticationUseCase.swift
+++ Projects/Application/Sources/Platforms/FirebaseAPIPlatform/UseCase/FirebaseAuthenticationUseCase.swift
@@ -29,14 +29,14 @@ struct FirebaseAuthenticationUseCase: AuthenticationUseCase {
       authenticating.create(email: domain.email, password: domain.password).asObservable(),
       mediaUploading.upload(image: domain.photo, directoryName: Const.directoryName).asObservable())
       .map { uid, imagePath -> (String, String, [String: Any]) in
-        let dictionanary: [String: Any] = [
+        let dictionary: [String: Any] = [
           "uid": uid,
           "email": domain.email,
           "profileImageURL": imagePath,
           "username": domain.userName,
           "fullname": domain.fullName,
         ]
-        return (uid, Const.directoryName, dictionanary)
+        return (uid, Const.collectionName, dictionary)
       }
       .flatMap { apiNetworking.create(uid: $0.0, collection: $0.1, dictionary: $0.2) }
   }
@@ -54,7 +54,7 @@ struct FirebaseAuthenticationUseCase: AuthenticationUseCase {
   // MARK: Private
 
   private struct Const {
-    static var directoryName = "user_profile"
+    static var directoryName = "profile_images"
     static var collectionName = "users"
   }
 
