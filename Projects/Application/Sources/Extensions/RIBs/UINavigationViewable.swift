11f767760cfea5466564756ed197577b64b63da7
Scott Moon <interactord@gmail.com>
2021-02-24 01:08:07 +0900

    추가 -> RIBs Viewable 네비게이션 공통 확장

---
+++ Projects/Application/Sources/Extensions/RIBs/UINavigationViewable.swift
@@ -0,0 +1,21 @@
+import RIBs
+import UIKit
+
+// MARK: - UINavigationViewable
+
+protocol UINavigationViewable {
+  func push(viewControllable: ViewControllable, animated: Bool)
+  func pop(viewControllable: ViewControllable, animated: Bool)
+}
+
+extension UINavigationViewable where Self: UIViewController {
+  func push(viewControllable: ViewControllable, animated: Bool) {
+    navigationController?.pushViewController(
+      viewControllable.uiviewController,
+      animated: animated)
+  }
+
+  func pop(viewControllable: ViewControllable, animated: Bool) {
+    dismiss(animated: animated)
+  }
+}
--- Projects/Application/Tests/RIBs/Mock/ViewControllableMock.swift
+++ Projects/Application/Tests/RIBs/Mock/ViewControllableMock.swift
@@ -1,13 +1,35 @@
-import UIKit
-
 import RIBs
+import UIKit
+@testable import Application
+
+// MARK: - ViewControllableMock
 
 class ViewControllableMock: ViewControllable {
+  var viewControllers = 0
   var uiViewControllerCallCount = 0
+  var pushCallCount = 0
+  var pushHandler: (() -> Void)?
+  var popCallCount = 0
+  var popHandler: (() -> Void)?
 
   var uiviewController = UIViewController() {
     didSet {
       uiViewControllerCallCount += 1
     }
+  }
+}
+
+// MARK: UINavigationViewable
+
+extension ViewControllableMock: UINavigationViewable {
+  func push(viewControllable: ViewControllable, animated: Bool) {
+    viewControllers += 1
+    pushCallCount += 1
+    pushHandler?()
+  }
+
+  func pop(viewControllable: ViewControllable, animated: Bool) {
+    popCallCount += 1
+    popHandler?()
   }
 }
