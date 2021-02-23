e9ce36b70067ff245e281695fac797d25ad20b41
Scott Moon <interactord@gmail.com>
2021-02-24 00:42:49 +0900

    추가 -> SubProfile 헤더 노드

---
+++ Projects/Application/Sources/RIBs/Sub/SubProfile/Nodes/SubProfileHeaderNode.swift
@@ -0,0 +1,76 @@
+import AsyncDisplayKit
+import BonMot
+
+// MARK: - SubProfileHeaderNode
+
+final class SubProfileHeaderNode: ASDisplayNode {
+
+  // MARK: Lifecycle
+
+  override init() {
+    super.init()
+    automaticallyManagesSubnodes = true
+    backgroundColor = .clear
+  }
+
+  deinit {
+    print("SubProfileHeaderNode deinit...")
+  }
+
+  // MARK: Private
+
+  private struct Const {
+    static var buttonSize = CGSize(width: 24, height: 24)
+    static var butrtonTintColor = UIColor.black
+    static var titleStyle = StringStyle(.font(.systemFont(ofSize: 13, weight: .bold)), .color(.black))
+    static let contentPadding = UIEdgeInsets(top: 12.5, left: 12, bottom: 12, right: 12)
+  }
+
+  private let titleNode: ASTextNode = {
+    let node = ASTextNode()
+    node.maximumNumberOfLines = 1
+    node.isLayerBacked = true
+    return node
+  }()
+
+  private let backButton: ASButtonNode = {
+    let node = ASButtonNode()
+    node.style.preferredSize = Const.buttonSize
+    node.setImage(#imageLiteral(resourceName: "back-icon"), for: .normal)
+    node.tintColor = Const.butrtonTintColor
+    return node
+  }()
+}
+
+// MARK: - LayoutSpec
+
+extension SubProfileHeaderNode {
+
+  // MARK: Internal
+
+  override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+    let contentLayout = ASStackLayoutSpec(
+      direction: .vertical,
+      spacing: .zero,
+      justifyContent: .start,
+      alignItems: .stretch,
+      children: [ contentAreaLayoutSpec() ])
+
+    return ASInsetLayoutSpec(insets: Const.contentPadding, child: contentLayout)
+  }
+
+  // MARK: Private
+
+  private func contentAreaLayoutSpec() -> ASLayoutSpec {
+    ASStackLayoutSpec(
+      direction: .horizontal,
+      spacing: .zero,
+      justifyContent: .spaceBetween,
+      alignItems: .center,
+      children: [
+        titleNode,
+        backButton,
+        ASLayoutSpec(),
+      ])
+  }
+}
