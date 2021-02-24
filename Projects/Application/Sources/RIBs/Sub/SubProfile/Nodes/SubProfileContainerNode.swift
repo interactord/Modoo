b18b808c1bea1af2b3f81c823a65e65e5bf7cdb9
Scott Moon <interactord@gmail.com>
2021-02-24 15:03:22 +0900

    추가 -> SubProfileHeaderNode ContainerNode에 연동

--- Projects/Application/Sources/RIBs/Sub/SubProfile/Nodes/SubProfileContainerNode.swift
+++ Projects/Application/Sources/RIBs/Sub/SubProfile/Nodes/SubProfileContainerNode.swift
@@ -4,20 +4,52 @@ import AsyncDisplayKit
 
 final class SubProfileContainerNode: ASDisplayNode {
 
+  // MARK: Lifecycle
+
   override init() {
     super.init()
+
     automaticallyManagesSubnodes = true
+    automaticallyRelayoutOnSafeAreaChanges = true
+    backgroundColor = .white
   }
 
   deinit {
     print("SubProfileContainerNode deinit...")
   }
+
+  // MARK: Internal
+
+  let headerNode = SubProfileHeaderNode()
+
+  let collectionNode: ASCollectionNode = {
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.scrollDirection = .vertical
+    flowLayout.minimumLineSpacing = .zero
+    flowLayout.minimumInteritemSpacing = .zero
+
+    let node = ASCollectionNode(collectionViewLayout: flowLayout)
+    node.registerSupplementaryNode(ofKind: UICollectionView.elementKindSectionHeader)
+    node.alwaysBounceVertical = true
+    node.style.flexGrow = 1
+    return node
+  }()
 }
 
 // MARK: - LayoutSpec
 
 extension SubProfileContainerNode {
   override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-    ASLayoutSpec()
+    let contentsLayout = ASStackLayoutSpec(
+      direction: .vertical,
+      spacing: .zero,
+      justifyContent: .start,
+      alignItems: .stretch,
+      children: [
+        headerNode,
+        collectionNode,
+      ])
+
+    return ASInsetLayoutSpec(insets: safeAreaInsets, child: contentsLayout)
   }
 }
