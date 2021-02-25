edcba39a0450f47dd076270cd609a63625b083e7
Scott Moon <interactord@gmail.com>
2021-02-26 05:25:51 +0900

    추가 -> Onboard RIBs 테스트 케이스 추가

--- Projects/Application/Tests/RIBs/Onboard/Mock/OnboardListenerMock.swift
+++ Projects/Application/Tests/RIBs/Onboard/Mock/OnboardListenerMock.swift
@@ -1,13 +1,28 @@
 @testable import Application
 
+// MARK: - OnboardListenerMock
+
 class OnboardListenerMock: OnboardListener {
 
   var routeToAuthenticationCallCount = 0
   var routeToAuthenticationHandler: (() -> Void)?
 
+  var routeToPostCallCount = 0
+  var routeToPostHandler: (() -> Void)?
+
   func routeToAuthentication() {
     routeToAuthenticationCallCount += 1
     routeToAuthenticationHandler?()
+  }
+
+}
+
+// MARK: OnboardPresentableListener
+
+extension OnboardListenerMock: OnboardPresentableListener {
+  func routeToPost() {
+    routeToPostCallCount += 1
+    routeToPostHandler?()
   }
 
 }
--- Projects/Application/Tests/RIBs/Onboard/OnboardViewControllerSpec.swift
+++ Projects/Application/Tests/RIBs/Onboard/OnboardViewControllerSpec.swift
@@ -7,12 +7,29 @@ class OnboardViewControllerSpec: QuickSpec {
   override func spec() {
     // swiftlint:disable implicitly_unwrapped_optional
     var viewController: OnboardViewController!
+    // swiftlint:disable implicitly_unwrapped_optional
+    var listener: OnboardListenerMock!
+    // swiftlint:disable implicitly_unwrapped_optional
+    var feedTypeViewController: NavigationController!
+    // swiftlint:disable implicitly_unwrapped_optional
+    var postTypeViewController: NavigationController!
+    // swiftlint:disable implicitly_unwrapped_optional
+    var selectedResult = false
 
     beforeEach {
+      feedTypeViewController = NavigationController(viewControllerType: .feed, image: nil, unselectedImage: nil, root: EmptyViewController())
+      postTypeViewController = NavigationController(viewControllerType: .post, image: nil, unselectedImage: nil, root: EmptyViewController())
+      listener = OnboardListenerMock()
       viewController = OnboardViewController()
+      viewController.viewControllers = [
+        feedTypeViewController,
+        postTypeViewController,
+      ]
+      viewController.listener = listener
     }
     afterEach {
       viewController = nil
+      listener = nil
     }
 
     describe("화면 로드가 완료되면") {
@@ -21,8 +38,29 @@ class OnboardViewControllerSpec: QuickSpec {
         viewController.viewDidLoad()
       }
 
-      it("ing...") {
-        expect(viewController).toNot(beNil())
+      context("feedTypeViewController를 선택했을 경우") {
+        beforeEach {
+          selectedResult = viewController.tabBarController(viewController, shouldSelect: feedTypeViewController)
+        }
+
+        it("selectedResult는 true이며, 화면이 전환이 된다") {
+          expect(selectedResult) == true
+        }
+      }
+
+      context("postTypeViewController를 선택했을 경우") {
+        beforeEach {
+          selectedResult = viewController.tabBarController(viewController, shouldSelect: postTypeViewController)
+        }
+
+        it("selectedResult는 false이며, 화면이 전환이 안된다") {
+          expect(selectedResult) == false
+
+        }
+
+        it("listener routeToPost를 호출한다") {
+          expect(listener.routeToPostCallCount) == 1
+        }
       }
     }
   }
